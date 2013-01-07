e9b057f9277636b8d429e0ad3249cde76c20359c
Chen Guo <chen.guo.0625@gmail.com>
2013-01-06 18:13:29 -0800

    Update index.js to not pollute global namespace. * js/index.js: wrap with self executing anonymous function.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
-loadAction(indexLoadAction);
+(function() {
+    var clearField = function () {
+        this.value = '';
+    };
 
-function indexLoadAction()
-{
-    // If browser hasn't populated the fields, replace them
-    var user = $("input[name='username']");
-    var pw = $("input[name='password']");
-    if ($(user).attr('value') == '')
-        $(user).attr('value', 'username');
-    if ($(pw).attr('value') == '')
-        $(pw).attr('value', 'password');
+    $(document).ready(function() {
+        // If browser hasn't populated the fields, replace them
+        var user = $("input[name='username']");
+        var pw = $("input[name='password']");
+        if ($(user).attr('value') == '')
+            $(user).attr('value', 'username');
+        if ($(pw).attr('value') == '')
+            $(pw).attr('value', 'password');
 
-    // Set up handlers to clear the field
-    $(user).click(clearField);
-    $(pw).click(clearField);
-}
-
-function clearField()
-{
-    this.value = '';
-}
+        // Set up handlers to clear the field
+        $(user).click(clearField);
+        $(pw).click(clearField);
+    });
+}) ();
